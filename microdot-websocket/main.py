ec6036fed73685c9248fc18ab3f47dbb23c37ea0
le LAN <58851935+lan-ensad@users.noreply.github.com>
2024-04-22 16:07:55 +0200

    web_update

--- microdot-websocket/main.py
+++ microdot-websocket/main.py
@@ -3,19 +3,19 @@ from microdot_websocket import with_websocket
 
 app = Microdot()
 
-
 @app.route('/')
 async def index(request):
     return send_file('index.html')
 
-
 @app.route('/echo')
+
 @with_websocket
 async def echo(request, ws):
     while True:
         data = await ws.receive()
         await ws.send(data)
-        print(type(data))
+        # print(f'{type(data)} : {data}')
+        print(data)
 
 
 app.run()
